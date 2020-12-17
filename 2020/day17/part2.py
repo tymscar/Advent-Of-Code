7582e3fdc62a4b22f22202accd8a0f2d236aa91e
tymscar <tymscar@gmail.com>
2020-12-17 19:03:55 +0000

    Optimised part 2 day 17

--- 2020/day17/part2.py
+++ 2020/day17/part2.py
@@ -1,24 +1,10 @@
 from collections import defaultdict
 
-def get_neighbours(of):
-    neighbours = []
-
-    for x in range(-1,2):
-        for y in range(-1,2):
-            for z in range(-1,2):
-                for w in range(-1,2):
-                    if x != 0 or y != 0 or z != 0 or w != 0:
-                        neighbours.append((of[0] + x, of[1] + y,of[2] + z, of[3] + w))
-
-    return neighbours
-
-
 def part_2():
     file = open('input.txt', 'r')
     world_one = defaultdict(lambda: ".")
     world_two = defaultdict(lambda: ".")
 
-    #neighbours = {}
 
     y = 0
     for line in file:
@@ -29,6 +15,11 @@ def part_2():
             world_two[(x,y,0,0)] = cube
             x+=1
         y += 1
+
+    minmax_x = [-1, x + 2]
+    minmax_y = [-1, y + 2]
+    minmax_z = [-1, 2]
+    minmax_w = [-1, 2]
 
     cycle = 0
     curr_world_num = 1
@@ -42,29 +33,48 @@ def part_2():
             this_world = world_one
             curr_world_num = 1
 
-        for x in range(0-(cycle+1), x + (cycle+2)):
-            for y in range(0 - (cycle+1), y + (cycle+2)):
-                for z in range(0-(cycle+1), (cycle+2)):
-                    for w in range(0-(cycle+1), (cycle+2)):
+        for x in range(minmax_x[0], minmax_x[1]):
+            for y in range(minmax_y[0], minmax_y[1]):
+                for z in range(minmax_z[0], minmax_z[1]):
+                    for w in range(minmax_w[0], minmax_w[1]):
                         curr_cube = (x,y,z,w)
 
                         active_neighbours = 0
-                        #if curr_cube in neighbours:
-                        #    neighs = neighbours[curr_cube]
-                        #else:
-                        #    neighs = get_neighbours(curr_cube)
-                        #    neighbours[curr_cube] = neighs
-                        for neighbor in get_neighbours(curr_cube):
-                            if this_world[neighbor] == "#":
-                                active_neighbours += 1
+
+                        for a in range(-1, 2):
+                            for b in range(-1, 2):
+                                for c in range(-1, 2):
+                                    for d in range(-1, 2):
+                                        if a != 0 or b != 0 or c != 0 or d != 0:
+                                            if this_world[(a + x, b + y, c + z, d + w)] == "#":
+                                                active_neighbours += 1
+
+
                         if this_world[curr_cube] == "#":
                             if active_neighbours < 2 or active_neighbours > 3:
                                 that_world[curr_cube] = "."
                             else:
                                 that_world[curr_cube] = "#"
+                                minmax_x[0] = min(minmax_x[0], curr_cube[0] - 2)
+                                minmax_y[0] = min(minmax_y[0], curr_cube[1] - 2)
+                                minmax_z[0] = min(minmax_z[0], curr_cube[2] - 2)
+                                minmax_w[0] = min(minmax_w[0], curr_cube[3] - 2)
+                                minmax_x[1] = max(minmax_x[1], curr_cube[0] + 2)
+                                minmax_y[1] = max(minmax_y[1], curr_cube[1] + 2)
+                                minmax_z[1] = max(minmax_z[1], curr_cube[2] + 2)
+                                minmax_w[1] = max(minmax_w[1], curr_cube[3] + 2)
+
                         else:
                             if active_neighbours == 3:
                                 that_world[curr_cube] = "#"
+                                minmax_x[0] = min(minmax_x[0], curr_cube[0] - 2)
+                                minmax_y[0] = min(minmax_y[0], curr_cube[1] - 2)
+                                minmax_z[0] = min(minmax_z[0], curr_cube[2] - 2)
+                                minmax_w[0] = min(minmax_w[0], curr_cube[3] - 2)
+                                minmax_x[1] = max(minmax_x[1], curr_cube[0] + 2)
+                                minmax_y[1] = max(minmax_y[1], curr_cube[1] + 2)
+                                minmax_z[1] = max(minmax_z[1], curr_cube[2] + 2)
+                                minmax_w[1] = max(minmax_w[1], curr_cube[3] + 2)
                             else:
                                 that_world[curr_cube] = "."
 
@@ -73,12 +83,8 @@ def part_2():
 
     if curr_world_num == 1:
         this_world = world_two
-        that_world = world_one
-        curr_world_num = 2
     else:
-        that_world = world_two
         this_world = world_one
-        curr_world_num = 1
 
     active_cubes = 0
     for cube in this_world:
