4c9e99b3a4d3379b2a5171966a19c371afe31f2e
tymscar <tymscar@gmail.com>
2020-12-25 12:47:17 +0000

    Finished day 25

--- 2020/day25/part1.py
+++ 2020/day25/part1.py
@@ -1,9 +1,25 @@
+
+def get_loop_size_for(subject_num, pub_key):
+    value = 1
+    loop_times = 0
+    while value != pub_key:
+        loop_times += 1
+        value *= subject_num
+        value = value % 20201227
+    
+    return loop_times
+
 def part_1():
-    file = open('input.txt', 'r')
+    card_pub, door_pub = [int(num) for num in open('input.txt').read().split("\n")]
 
-    for line in file:
-        print(line)
+    card_loop_size = get_loop_size_for(7, card_pub)
 
-    return "Done"
+    answer = 1
+
+    for _ in range(card_loop_size):
+        answer *= door_pub
+        answer = answer % 20201227
+
+    return answer
 
 print(part_1())
