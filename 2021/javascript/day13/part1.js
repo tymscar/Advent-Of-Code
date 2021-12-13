1562680aadb074107a0d3a7fbf36bc10d0087d95
Oscar Molnar <tymscar@gmail.com>
2021-12-13 13:06:40 +0000

    finished 2021 day 13 both parts

---
+++ 2021/javascript/day13/part1.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+const inputData = fs.readFileSync('input.txt', 'utf8').split('\n\n').map(a => a.split('-'));
+
+const initialPoints = inputData[0].map(a => a.split('\n').map(b => b.split(',').map(Number))).flat()
+const folds = inputData[1].map(a => a.split('\n').map(a => {
+    const splitLine = a.split('=');
+    return [splitLine[0].at(-1), Number(splitLine[1])]
+})).flat()
+
+let width = Math.max(...initialPoints.map(a => a[0])) + 1;
+let height = Math.max(...initialPoints.map(a => a[1])) + 1;
+
+let map = Array(width).fill().map(() => Array(height).fill(false));
+
+initialPoints.forEach(point => map[point[0]][point[1]] = true);
+
+[folds[0]].forEach(fold => {
+    if (fold[0] == 'y') {
+        height = fold[1];
+        const newMap = Array(width).fill().map(() => Array(height).fill(false));
+        for (let x = 0; x < width; x++) {
+            for (let diff = 1; diff <= height; diff++) {
+                const oldTopVal = map[x][height - diff] || false;
+                const oldBotVal = map[x][height + diff] || false;
+                newMap[x][height - diff] = oldBotVal || oldTopVal;
+            }
+        }
+        map = newMap;
+    } else {
+        width = fold[1];
+        const newMap = Array(width).fill().map(() => Array(height).fill(false));
+        for (let y = 0; y < height; y++) {
+            for (let diff = 1; diff <= width; diff++) {
+                const oldLeftVal = map[width - diff][y] || false;
+                const oldRightVal = map[width + diff][y] || false;
+                newMap[width - diff][y] = oldLeftVal || oldRightVal;
+            }
+        }
+        map = newMap;
+    }
+})
+
+const visibleDots = map.flat().reduce((total, curr) => total + (curr ? 1 : 0), 0);
+
+console.log(visibleDots);
