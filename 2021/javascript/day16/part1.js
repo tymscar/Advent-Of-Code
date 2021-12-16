c666fc05f504ef3626ec4db524b3fcd59e52351a
Oscar Molnar <tymscar@gmail.com>
2021-12-16 12:29:04 +0000

    finished 2021 day 16 part1

---
+++ 2021/javascript/day16/part1.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+const hex2bin = hex => {
+	const binaryArray = [];
+	hex.split('').forEach(char =>{
+		binaryArray.push(parseInt(char, 16).toString(2).padStart(4, '0'));
+	});
+	return binaryArray.join('');
+};
+
+const input = hex2bin(fs.readFileSync('input.txt', 'utf8'));
+
+
+const getPackets = bin => {
+	const packets = [];
+	let index = 0;
+	while (bin.length - index >= 11) {
+
+		const packet = {};
+		packet.version = parseInt(bin.substr(0 + index, 3), 2);
+		packet.type = parseInt(bin.substr(3 + index, 3), 2);
+		if (packet.type === 4) { // value
+			const bitsRead = [];
+			let readPos = 6 + index;
+			let infoBit = '0';
+			do {
+				infoBit = bin.substr(readPos, 1);
+				bitsRead.push(bin.substr(readPos + 1, 4));
+				readPos += 5;
+			} while (readPos + 5 <= bin.length && infoBit !== '0');
+			packet.data = parseInt(bitsRead.join(''), 2);
+			index = readPos;
+		} else { // operator
+
+			if (bin.substr(6 + index, 1) === '0') { // next 15 bits
+
+				const lengthSubPackets = parseInt(bin.substr(7 + index, 15), 2);
+				const subPacketString = bin.substr(22 + index, lengthSubPackets);
+				const subPackets = getPackets(subPacketString);
+
+				subPackets.forEach(subPack => packets.push(subPack));
+				index = 22 +index + lengthSubPackets;
+			} else { // next 11 bits
+
+				const howManySubPackets = parseInt(bin.substr(7 + index, 11), 2);
+				let subPacketStringLength = bin.length - (18 + index);
+				let subPackets = [];
+				while (subPackets.length < howManySubPackets){
+					subPackets = getPackets(bin.substr(18 + index, subPacketStringLength));
+				}
+
+				subPackets.forEach(subPack => packets.push(subPack));
+				index = 18 +index+ subPacketStringLength;
+			}
+		}
+
+
+
+
+		packets.push(packet);
+	}
+
+	return packets;
+};
+
+const versionSum = getPackets(input).reduce((total, curr) => total = total + curr.version, 0);
+
+console.log(versionSum);
