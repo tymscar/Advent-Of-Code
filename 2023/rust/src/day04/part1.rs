571f68003f0f74108cdddbc7bef9091ff224c0e8
Tymscar <oscar@tymscar.com>
2023-12-04 06:02:11 +0000

    Finished 2023 day 4 both parts

--- 2023/rust/src/day04/part1.rs
+++ 2023/rust/src/day04/part1.rs
@@ -1,7 +1,30 @@
+use std::collections::HashSet;
+
+fn calculate_card_worth(card: &str) -> usize {
+    let card: Vec<_> = card.split(": ").collect();
+    let card: Vec<_> = card[1].split(" | ").collect();
+    let winning_numbers: HashSet<_> = card[0].split(" ").filter(|x| x.len() > 0).collect();
+    let played_numbers: Vec<_> = card[1].split(" ").filter(|x| x.len() > 0).collect();
+
+    let mut matches: u32 = 0;
+    for number in played_numbers {
+        if winning_numbers.contains(number) {
+            matches += 1;
+        }
+    }
+
+    match matches {
+        0 => 0,
+        _ => 2_usize.pow(matches - 1) as usize,
+    }
+}
+
 pub fn part1(input: &str) -> String {
     let lines: Vec<_> = input.split('\n').collect();
-    
-	println!("{:?}", lines);
+    let points: Vec<_> = lines
+        .iter()
+        .map(|line| calculate_card_worth(line))
+        .collect();
 
-    "".to_string()
+    points.iter().sum::<usize>().to_string()
 }
---
+++ 2023/rust/src/day04/part2.rs
@@ -0,0 +1,36 @@
+use std::collections::{HashMap, HashSet};
+
+fn calculate_card_matches(card: &str) -> usize {
+    let card: Vec<_> = card.split(": ").collect();
+    let card: Vec<_> = card[1].split(" | ").collect();
+    let winning_numbers: HashSet<_> = card[0].split(" ").filter(|x| x.len() > 0).collect();
+    let played_numbers: Vec<_> = card[1].split(" ").filter(|x| x.len() > 0).collect();
+
+    played_numbers
+        .iter()
+        .fold(0, |acc, &number| match winning_numbers.contains(&number) {
+            true => acc + 1,
+            false => acc,
+        })
+}
+
+pub fn part2(input: &str) -> String {
+    let lines: Vec<_> = input.split('\n').collect();
+    let matches: Vec<_> = lines
+        .iter()
+        .map(|line| calculate_card_matches(line))
+        .collect();
+
+    let mut card_amounts: HashMap<usize, usize> = (0..lines.len()).map(|i| (i, 1)).collect();
+
+    for (i, matches) in matches.iter().enumerate() {
+        let curr_amount = card_amounts.get(&i).unwrap().clone();
+        for index in i + 1..i + matches + 1 {
+            if let Some(amount) = card_amounts.get_mut(&(index)) {
+                *amount += curr_amount;
+            }
+        }
+    }
+
+    card_amounts.values().sum::<usize>().to_string()
+}
