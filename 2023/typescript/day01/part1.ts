434433841a3ed52d4d6b8bd287d4d00a084a2d73
Tymscar <oscar@tymscar.com>
2023-12-10 16:10:07 +0000

    Remove typescript from day 1 2023

--- 2023/typescript/day01/part1.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-const sum = (a: number, b: number): number => a + b;
-
-const input = fs.readFileSync('inputs/day01.txt', 'utf8').split('\n');
-const numbers = input.map(line => line.split("").filter(char =>  Number.isFinite(+char))).map(digits => digits[0] + digits[digits.length-1])
-const answer = numbers.map(number => +number).reduce(sum, 0)
-
-console.log(answer);
